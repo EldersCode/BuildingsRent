0a3e17ac9e70fbe56b9b7a51ceec01f8aaaf3f41
HeshamMorsy <Hesham0hero@gmail.com>
2017-08-05 03:19:58 +0200

    first profile design

---
+++ app/src/main/java/com/profile/activities/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.profile.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.buildingsrent.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
