f9895eaee1ca509456404ff4f45788de6b219b5a
HeshamMorsy <Hesham0hero@gmail.com>
2017-08-17 19:50:53 +0200

    making advertisement activity

---
+++ app/src/main/java/com/project/buildingsrent/AdvertiseActivity.java
@@ -0,0 +1,13 @@
+package com.project.buildingsrent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdvertiseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertise);
+    }
+}
